be88e27f48391183f05785c3440eea776b60c438
Sorato Saeki <sokominto1231@gmail.com>
2023-08-09 03:30:23 +0000

    add: PlusBtn

---
+++ src/components/Plus.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <button type="submit"><router-link to='/post' class="plus"><i class="pencil alternate icon"></i></router-link></button>
+    </div>
+</template>
+
+<style scoped>
+    div {
+        text-align: right;
+    }
+    
+    button {
+        margin: 20px;
+        padding: 10px;
+    }
+    
+    .plus {
+        color: #ffffff;
+        font-weight: bold;
+        text-decoration: none;
+    }
+</style>
