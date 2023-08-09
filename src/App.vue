236a1cc362a24191b280b5d8bc2f4fb00ad32a3a
Sorato Saeki <sokominto1231@gmail.com>
2023-08-09 03:25:16 +0000

    fix: 投稿ボタンの位置修正

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <router-view />
     </main>
   </div>
+  <Plus class='plus-box' v-if="!['Lp', 'Login', 'Diagnosehome', 'Diagnose', 'Negative', 'Positive'].includes($route.name)"/>
   <Footer />
 </template>
 <script>
@@ -12,12 +13,14 @@
 import { RouterView } from 'vue-router'
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+import Plus from '@/components/Plus.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
-    Footer
+    Footer,
+    Plus
   },
   computed: {
     isMenuShow() {
@@ -36,5 +39,15 @@ export default {
   main {
     margin: 0 15px;
     min-height: calc(100vh - 80px - 60px);
+  }
+  .plus-box {
+    position: fixed;
+    bottom: 5em;
+    width: 500px;
+  }
+  @media screen and (max-width: 500px) {
+    .plus-box {
+      width: 100%;
+    }
   }
 </style>
